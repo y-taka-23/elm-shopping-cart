d9c2feb4691b88759fd50c0913f77f6339b37bca
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2019-06-08 23:08:38 +0900

    Define the model

--- src/elm/Model.elm
+++ src/elm/Model.elm
@@ -1,4 +1,14 @@
-module Model exposing (Model, Msg(..), initModel)
+module Model exposing
+    ( Cart
+    , CartItem
+    , CheckoutStatus(..)
+    , Model
+    , Msg(..)
+    , Product
+    , initModel
+    )
+
+import Dict exposing (Dict)
 
 
 type Msg
@@ -6,9 +16,50 @@ type Msg
 
 
 type alias Model =
-    {}
+    { products : List Product
+    , loading : Bool
+    , cart : Cart
+    , checkoutStatus : Maybe CheckoutStatus
+    }
+
+
+type alias Product =
+    { id : ProductId
+    , title : String
+    , price : Float
+    , inventory : Quantity
+    }
+
+
+type alias ProductId =
+    Int
+
+
+type alias Quantity =
+    Int
+
+
+type alias Cart =
+    Dict ProductId Quantity
+
+
+type alias CartItem =
+    { id : ProductId
+    , title : String
+    , price : Float
+    , quantity : Quantity
+    }
+
+
+type CheckoutStatus
+    = Success
+    | Fail
 
 
 initModel : Model
 initModel =
-    {}
+    { products = []
+    , loading = True
+    , cart = Dict.empty
+    , checkoutStatus = Nothing
+    }
